2c6ea4b19eb220648f7860e78a56ac554b94de71
Kenton <38125586+Kenton1989@users.noreply.github.com>
2021-01-24 00:40:57 +0800

    Refine dynamic page backend

--- js/dynamic-page-backend.js
+++ js/dynamic-page-backend.js
@@ -1,5 +1,5 @@
 class DynamicPageBackend {
-  static #tabArgs = {};
+  static #tabArgs = new Map();
   static #setup = false;
 
   static setup() {
@@ -8,11 +8,23 @@ class DynamicPageBackend {
     DynamicPageBackend.#setup = true;
 
     // send arguments back on request.
-    chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
+    chrome.runtime.onMessage.addListener(function (
+      message,
+      sender,
+      sendResponse
+    ) {
       if (message.dynamic_page_init_request) {
         let tabId = sender.tab.id;
-        let args = DynamicPageBackend.#tabArgs[tabId];
-        sendResponse({dynamic_page_init_args: args});
+        let args = DynamicPageBackend.#tabArgs.get(tabId);
+        sendResponse({ dynamic_page_init_args: args });
+      }
+    });
+
+    // remove args of tab on close
+    chrome.tabs.onRemoved.addListener(function (tabId, removeInfo) {
+      console.log(`Tab #${tabId} closed.`);
+      if (DynamicPageBackend.#tabArgs.delete(tabId)) {
+        console.log(`Argument for tab #${tabId} deleted.`);
       }
     });
   }
@@ -25,24 +37,31 @@ class DynamicPageBackend {
    * @param {Number} tabId tab id of the page on which the page is opened.
    * @param {function(any)} callback the callback after the tab is created. 1st parameter is updated tab object.
    */
-  static openOnExistingTab(url, pageArgs, tabId, callback = function(tab){}) {
-    DynamicPageBackend.#tabArgs[tabId] = pageArgs;
+  static openOnExistingTab(url, pageArgs, tabId, callback = function (tab) {}) {
+    DynamicPageBackend.#tabArgs.set(tabId, pageArgs);
     chrome.tabs.update(tabId, { url: url }, callback);
+    console.log(`Overwriting tab #${tabId} with URL:${url}.`);
   }
 
   /**
    * Open the given dynamic page with the given arguments on a new tab.
-   * 
+   *
    * @param {String} url the url to the dynamic page to be opened
    * @param {any} pageArgs the arguments passed to the dynamic page
-   * @param {*} tabProperties the properties of the new tab passed to the method 
+   * @param {*} tabProperties the properties of the new tab passed to the method
    * chrome.tabs.create(). The tabProperties.url will be ignored if it is defined.
    * @param {function(any)} callback the callback after the tab is created. 1st parameter is newly created tab object.
    */
-  static openOnNewTab(url, pageArgs, tabProperties = {}, callback = function(tab){}) {
+  static openOnNewTab(
+    url,
+    pageArgs,
+    tabProperties = {},
+    callback = function (tab) {}
+  ) {
     tabProperties.url = url;
-    chrome.tabs.create(tabProperties, function(tab) {
-      DynamicPageBackend.#tabArgs[tab.id] = pageArgs;
+    chrome.tabs.create(tabProperties, function (tab) {
+      DynamicPageBackend.#tabArgs.set(tab.id, pageArgs);
+      console.log(`Created tab #${tab.id} opened with URL:${url}.`);
       callback(tab);
     });
   }
