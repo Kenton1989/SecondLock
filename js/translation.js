358909e059d5f65448cdbcabfd740fb6b927766a
Kenton <38125586+Kenton1989@users.noreply.github.com>
2021-03-07 17:27:56 +0800

    translate options page

--- js/translation.js
+++ js/translation.js
@@ -1,7 +1,19 @@
 import { TRANS_KEYS } from "./trans-keys.js";
 
-function generalTranslate() {
-  for (const key of TRANS_KEYS) {
+const SUPPORTED_LANG = new Set(["zh-CN"]);
+
+function canTranslate() {
+  let langCode = chrome.i18n.getUILanguage();
+  console.debug("Lang code: " + langCode);
+  return SUPPORTED_LANG.has(langCode);
+}
+
+function generalTranslate(transKeys = TRANS_KEYS) {
+  if (!canTranslate()) {
+    console.debug(`No translation applied.`);
+    return;
+  }
+  for (const key of transKeys) {
     let elements = document.getElementsByClassName(`trans-${key}`);
     let txt = chrome.i18n.getMessage(key);
     for (const ele of elements) {
