7685c03565af8ed93992c5182088fa75d84cd0c6
Kenton <38125586+Kenton1989@users.noreply.github.com>
2021-03-26 20:25:31 +0800

    minor changes

--- src/pages/popup/Popup.jsx
+++ src/pages/popup/Popup.jsx
@@ -1,5 +1,4 @@
-import React, { useState } from "react";
-import { render } from "react-dom";
+import React from "react";
 import { api } from "../../common/api";
 import { RemoteCallable } from "../../common/remote-callable";
 import { $t } from "../../common/utility";
